7136e071108d662a212c5921a1c8e11d1546088b
bardocku <bardocku@yahoo.com>
2021-04-07 10:47:12 +0200

    Initial app

---
+++ mathfunc.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+
+import wx
+import matplotlib as mpl
+from matplotlib import figure
+from matplotlib.backends.backend_wxagg import (
+    FigureCanvasWxAgg as FigureCanvas,
+    NavigationToolbar2WxAgg as NavigationToolbar)
+import wx.lib.agw.aui as aui
+import numpy as np
+from scipy import signal
+from scipy.signal import argrelextrema
+import math
+
+
+class Meas:
+    def __init__(self, sig):
+        self.sig = sig
+
+    def ValMax(self):
+        v_max = np.max(self.sig)
+        return round(v_max, 5)
+
+    def ValMin(self):
+        v_min = np.min(self.sig)
+        return round(v_min, 5)
+
+    def ValPeak(self):
+        v_peak = abs(self.ValMin())+abs(self.ValMax())
+        return round(v_peak, 5)
+
+    def ValMid(self):
+        v_mid = (self.ValMin()+self.ValMax())/2
+        return round(v_mid, 5)
+
+    def ValAvg(self):
+        v_avg = np.average(self.sig)
+        return round(v_avg, 5)
+
+    def ValACRMS(self):
+        v = 0
+        for i in range(len(self.sig)):
+            v = v + (self.sig[i]-self.ValAvg())**2
+        v = v/len(self.sig)
+        v_RMS = np.sqrt(v)
+        return round(v_RMS, 5)
+
+    def ValDCRMS(self):
+        v = 0
+        for i in range(len(self.sig)):
+            v = v + (self.sig[i] ** 2)
+        v = v/len(self.sig)
+        v_RMS = np.sqrt(v)
+        return round(v_RMS, 5)
+
+    def ValAmp(self):
+        v_amp = (self.ValMax() - self.ValMin())/2
+        return round(v_amp, 5)
+
+    def ValOver(self):
+        v = self.ValAmp()
+        if math.isnan(v) or v == 0:
+            return 0
+        else:
+            return round(100 * (self.ValPeak() / v / 2 - 1), 4)
+
+    def PosDuty(self):
+        v_sum = sum(i > self.ValMid() for i in self.sig)/len(self.sig)
+        return round(v_sum, 4)
+
+    def NegDuty(self):
+        v_sum = 1 - self.PosDuty()
+        return round(v_sum, 4)
+
+
+class FloatSlider(wx.Slider):
+    def __init__(self, parent, id, value, minval, maxval, res,
+                 size=wx.DefaultSize, style=wx.SL_HORIZONTAL,
+                 name='floatslider'):
+        self._value = value
+        self._min = minval
+        self._max = maxval
+        self._res = res
+        ival, imin, imax = [round(v / res) for v in (value, minval, maxval)]
+        self._islider = super(FloatSlider, self)
+        self._islider.__init__(
+            parent, id, ival, imin, imax, size=size, style=style, name=name
+        )
+        self.Bind(wx.EVT_SCROLL, self._OnScroll)
+
+    def _OnScroll(self, event):
+        ival = self._islider.GetValue()
+        imin = self._islider.GetMin()
+        imax = self._islider.GetMax()
+        if ival == imin:
+            self._value = self._min
+        elif ival == imax:
+            self._value = self._max
+        else:
+            self._value = ival * self._res
+        event.Skip()
+
+    # print 'OnScroll: value=%f, ival=%d' % (self._value, ival)
+
+    def GetValue(self):
+        return self._value
+
+    def GetMin(self):
+        return self._min
+
+    def GetMax(self):
+        return self._max
+
+    def GetRes(self):
+        return self._res
+
+    def SetValue(self, value):
+        self._islider.SetValue(round(value / self._res))
+        self._value = value
+
+    def SetMin(self, minval):
+        self._islider.SetMin(round(minval / self._res))
+        self._min = minval
+
+    def SetMax(self, maxval):
+        self._islider.SetMax(round(maxval / self._res))
+        self._max = maxval
+
+    def SetRes(self, res):
+        self._islider.SetRange(round(self._min / res), round(self._max / res))
+        self._islider.SetValue(round(self._value / res))
+        self._res = res
+
+    def SetRange(self, minval, maxval):
+        self._islider.SetRange(round(minval / self._res), round(maxval / self._res))
+        self._min = minval
+        self._max = maxval
+
+
+class Plot(wx.Panel):
+    def __init__(self, parent, id=-1, dpi=None, **kwargs):
+        super().__init__(parent, id=id, **kwargs)
+        self.figure = mpl.figure.Figure(dpi=dpi, figsize=(2, 2))
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.toolbar = NavigationToolbar(self.canvas)
+        self.toolbar.Realize()
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.canvas, 1, wx.EXPAND)
+        sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
+        self.Centre()
+        self.SetSizer(sizer)
+
+
+class PlotNotebook(wx.Panel):
+    def __init__(self, parent, id=-1):
+        super().__init__(parent, id=id)
+        self.nb = aui.AuiNotebook(self)
+        self.cord = 0
+        sizer = wx.BoxSizer()
+        pnl = wx.Panel(self)
+        meas_pnl = wx.Panel(self)
+        chnl_pnl = wx.Panel(self)
+
+        # Tworzenie elementów interfejsu
+        self.button = wx.Button(self, wx.ID_ANY, 'Apply', (10, 10))
+        self.button1 = wx.Button(self, wx.ID_ANY, 'Change signal', (10, 10))
+        self.button2 = wx.Button(self, wx.ID_ANY, 'CH2 Enable')
+        self.btnch1 = wx.Button(chnl_pnl, wx.ID_ANY, 'CH1')
+        self.btnch2 = wx.Button(chnl_pnl, wx.ID_ANY, 'CH2')
+
+        self.fslider0 = FloatSlider(self, -1, 1, 1, 100, 1)
+        self.fslider1 = FloatSlider(self, -1, 1, 1, 100, 1)
+        self.fslider2 = FloatSlider(self, -1, 1, 1, 100, 1)
+        self.fslider3 = FloatSlider(self, -1, 1, 1, 3, 1)
+
+        self.txt0 = wx.StaticText(pnl, label='1')
+        self.txt1 = wx.StaticText(pnl, label='1')
+        self.txt2 = wx.StaticText(pnl, label='1')
+        self.txt3 = wx.StaticText(pnl, label='1')
+        self.txt4 = wx.StaticText(pnl, label='0')
+
+        self.vmin = wx.StaticText(meas_pnl, label='1')
+        self.vmax = wx.StaticText(meas_pnl, label='1')
+        self.vpeak = wx.StaticText(meas_pnl, label='1')
+        self.vmid = wx.StaticText(meas_pnl, label='1')
+        self.vavg = wx.StaticText(meas_pnl, label='1')
+        self.vacrms = wx.StaticText(meas_pnl, label='1')
+        self.vdcrms = wx.StaticText(meas_pnl, label='1')
+        self.vamp = wx.StaticText(meas_pnl, label='1')
+        self.vover = wx.StaticText(meas_pnl, label='0')
+        self.vpduty = wx.StaticText(meas_pnl, label='0 %')
+        self.vnduty = wx.StaticText(meas_pnl, label='0 %')
+        self.freq = wx.StaticText(meas_pnl, label='0 Hz')
+
+        self.chnlid = wx.StaticText(chnl_pnl, label='CH1')
+
+        # Deklaracja układu elementów
+        meas_sizer = wx.GridBagSizer(1, 1)
+        pnl_sizer = wx.GridBagSizer(1, 1)
+        chnl_sizer = wx.GridBagSizer(1, 1)
+
+        # Rozmieszczanie elementów w danych panelach
+
+        # Elementy tekstowe
+        pnl_sizer.Add(wx.StaticText(pnl, label='FFT'), pos=(0, 0), flag=wx.LEFT, border=10)
+        pnl_sizer.Add(wx.StaticText(pnl, label='AMP'), pos=(3, 0), flag=wx.LEFT, border=10)
+        pnl_sizer.Add(wx.StaticText(pnl, label='FRQ'), pos=(5, 0), flag=wx.LEFT, border=10)
+        pnl_sizer.Add(wx.StaticText(pnl, label='ST'), pos=(7, 0), flag=wx.LEFT, border=10)
+
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Maximum'), pos=(0, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Minimum'), pos=(1, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Peak2Peak'), pos=(2, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Middle'), pos=(3, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Average'), pos=(4, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='AC RMS'), pos=(5, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='DC RMS'), pos=(6, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Amplitude'), pos=(7, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Overshoot'), pos=(8, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Pos Duty'), pos=(9, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Neg Duty'), pos=(10, 0), flag=wx.LEFT, border=10)
+        meas_sizer.Add(wx.StaticText(meas_pnl, label='Freq'), pos=(11, 0), flag=wx.LEFT, border=10)
+
+        # Elementy tekstowe zmienne
+        pnl_sizer.Add(self.txt0, pos=(1, 1), flag=wx.RIGHT, border=10)
+        pnl_sizer.Add(self.txt1, pos=(4, 1), flag=wx.RIGHT, border=10)
+        pnl_sizer.Add(self.txt2, pos=(6, 1), flag=wx.RIGHT, border=10)
+        pnl_sizer.Add(self.txt3, pos=(8, 1), flag=wx.RIGHT, border=10)
+        pnl_sizer.Add(self.txt4, pos=(11, 1), flag=wx.RIGHT, border=10)
+
+        meas_sizer.Add(self.vmax, pos=(0, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vmin, pos=(1, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vpeak, pos=(2, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vmid, pos=(3, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vavg, pos=(4, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vacrms, pos=(5, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vdcrms, pos=(6, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vamp, pos=(7, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vover, pos=(8, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vpduty, pos=(9, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.vnduty, pos=(10, 1), flag=wx.RIGHT, border=10)
+        meas_sizer.Add(self.freq, pos=(11, 1), flag=wx.RIGHT, border=10)
+
+        chnl_sizer.Add(self.chnlid, pos=(0, 1), flag=wx.ALL | wx.EXPAND, border=10)
+
+        # Elemementy interaktywne
+        pnl_sizer.Add(self.fslider0, pos=(1, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.fslider1, pos=(4, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.fslider2, pos=(6, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.fslider3, pos=(8, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.button, pos=(2, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.button1, pos=(9, 0), flag=wx.ALL | wx.EXPAND, border=1)
+        pnl_sizer.Add(self.button2, pos=(11, 0), flag=wx.EXPAND, border=1)
+
+        chnl_sizer.Add(self.btnch1, pos=(0, 0), flag=wx.SHAPED, border=10)
+        chnl_sizer.Add(self.btnch2, pos=(0, 2), flag=wx.SHAPED, border=10)
+
+        chnl_sizer.AddGrowableCol(0)
+        chnl_sizer.SetMinSize(200, 100)
+        chnl_pnl.SetSizer(chnl_sizer)
+
+        meas_sizer.AddGrowableCol(0)
+        meas_sizer.SetMinSize(200, 100)
+        meas_pnl.SetSizer(meas_sizer)
+        pnl_sizer.Add(chnl_pnl, pos=(13, 0), flag=wx.ALL | wx.EXPAND)
+        pnl_sizer.Add(meas_pnl, pos=(12, 0), flag=wx.ALL | wx.EXPAND)
+
+        pnl_sizer.AddGrowableCol(0)
+        pnl_sizer.SetMinSize(200, 100)
+        pnl.SetSizer(pnl_sizer)
+        sizer.Add(pnl, 0, wx.SHAPED)
+
+        sizer.Add(self.nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+    def RefVal(self, event):
+        self.txt0.SetLabel(str(self.fslider0.GetValue()))
+        self.txt1.SetLabel(str(self.fslider1.GetValue()))
+        self.txt2.SetLabel(str(self.fslider2.GetValue()))
+        self.txt3.SetLabel(str(self.fslider3.GetValue()))
+        self.fslider0.Bind(wx.EVT_SCROLL, self.fslider0._OnScroll)
+        self.fslider1.Bind(wx.EVT_SCROLL, self.fslider1._OnScroll)
+        self.fslider2.Bind(wx.EVT_SCROLL, self.fslider2._OnScroll)
+        self.fslider3.Bind(wx.EVT_SCROLL, self.fslider3._OnScroll)
+
+    def add(self, name="plot"):
+        page = Plot(self.nb)
+        self.nb.AddPage(page, name)
+        return page.figure
+
+    def delI(self, id):
+        self.nb.DeletePage(id)
+
+
+def switch_demo(argument, hz, am, st, x):
+    y = {
+        1: am * np.sin(hz * 2.0 * np.pi * x) - st,
+        2: am * signal.square(hz * 2.0 * np.pi * x, duty=0.5) - st,
+        3: am * signal.sawtooth(hz * 2.0 * np.pi * x) - st
+    }
+    return y.get(argument)
+
+
+class FFT_Func:
+    def __init__(self, y, N, T, res):
+        self.y = y
+        self.N = N
+        self.T = T
+        self.res = res
+
+    def FFT_Y(self):
+        yf = np.fft.fft(self.y)
+        return yf
+
+    def FFT_X(self):
+        xf = np.fft.fftfreq(self.N, self.T)[:self.N // 2]
+        return xf
+
+    def FFT_Freq(self):
+        z = argrelextrema(2.0 / self.N * np.abs(self.FFT_Y()[0:self.N // 2]), np.greater, 0, self.res)
+        z = np.concatenate(np.asarray(z))
+        return z
+
+    def FFT_Freq_Y(self):
+        zy = np.ones(len(self.FFT_Freq())) * (2.0 / self.N * np.abs(self.FFT_Y()[self.FFT_Freq()]))
+        # s = np.zeros(len(z))
+        return zy
--- testapp.py
+++ testapp.py
@@ -1,176 +1,16 @@
 #!/usr/bin/env python3
 
 import wx
-
-import matplotlib as mpl
-from matplotlib.backends.backend_wxagg import (
-    FigureCanvasWxAgg as FigureCanvas,
-    NavigationToolbar2WxAgg as NavigationToolbar)
-
-import wx.lib.agw.aui as aui
-import wx.lib.mixins.inspection as wit
-
 import numpy as np
 import scipy as sp
-from scipy import signal
-from scipy.signal import argrelextrema
-
-
-def switch_demo(argument, hz, am, st):
-    y = {
-        1: am * np.sin(hz * 2.0 * np.pi * x) + st,
-        2: am * signal.square(hz * 2.0 * np.pi * x) + st,
-        3: am * signal.sawtooth(1.0 * 2.0 * np.pi * x) + st
-    }
-    return y.get(argument)
-
-
-class FloatSlider(wx.Slider):
-    def __init__(self, parent, id, value, minval, maxval, res,
-                 size=wx.DefaultSize, style=wx.SL_HORIZONTAL,
-                 name='floatslider'):
-        self._value = value
-        self._min = minval
-        self._max = maxval
-        self._res = res
-        ival, imin, imax = [round(v / res) for v in (value, minval, maxval)]
-        self._islider = super(FloatSlider, self)
-        self._islider.__init__(
-            parent, id, ival, imin, imax, size=size, style=style, name=name
-        )
-        self.Bind(wx.EVT_SCROLL, self._OnScroll)
-
-    def _OnScroll(self, event):
-        ival = self._islider.GetValue()
-        imin = self._islider.GetMin()
-        imax = self._islider.GetMax()
-        if ival == imin:
-            self._value = self._min
-        elif ival == imax:
-            self._value = self._max
-        else:
-            self._value = ival * self._res
-        event.Skip()
-
-    # print 'OnScroll: value=%f, ival=%d' % (self._value, ival)
-
-    def GetValue(self):
-        return self._value
-
-    def GetMin(self):
-        return self._min
-
-    def GetMax(self):
-        return self._max
-
-    def GetRes(self):
-        return self._res
-
-    def SetValue(self, value):
-        self._islider.SetValue(round(value / self._res))
-        self._value = value
-
-    def SetMin(self, minval):
-        self._islider.SetMin(round(minval / self._res))
-        self._min = minval
-
-    def SetMax(self, maxval):
-        self._islider.SetMax(round(maxval / self._res))
-        self._max = maxval
-
-    def SetRes(self, res):
-        self._islider.SetRange(round(self._min / res), round(self._max / res))
-        self._islider.SetValue(round(self._value / res))
-        self._res = res
-
-    def SetRange(self, minval, maxval):
-        self._islider.SetRange(round(minval / self._res), round(maxval / self._res))
-        self._min = minval
-        self._max = maxval
-
-
-class Plot(wx.Panel):
-
-    def __init__(self, parent, id=-1, dpi=None, **kwargs):
-        super().__init__(parent, id=id, **kwargs)
-        self.figure = mpl.figure.Figure(dpi=dpi, figsize=(2, 2))
-        self.canvas = FigureCanvas(self, -1, self.figure)
-        self.toolbar = NavigationToolbar(self.canvas)
-        self.toolbar.Realize()
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.canvas, 1, wx.EXPAND)
-        sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
-        self.Centre()
-        self.SetSizer(sizer)
-
-
-class PlotNotebook(wx.Panel):
-    idzed = 0
-    def __init__(self, parent, id=-1):
-        super().__init__(parent, id=id)
-        self.nb = aui.AuiNotebook(self)
-        sizer = wx.BoxSizer()
-        pnl = wx.Panel(self)
-
-        self.button = wx.Button(self, wx.ID_ANY, 'Apply', (10, 10))
-        # self.button.Bind(wx.EVT_BUTTON, onButton)
-
-        self.button1 = wx.Button(self, wx.ID_ANY, 'Change signal', (10, 10))
-        # self.button.Bind(wx.EVT_BUTTON, onButton)
-
-        self.fslider0 = FloatSlider(self, -1, 1, 1, 100, 1)
-        self.fslider1 = FloatSlider(self, -1, 1, 1, 100, 1)
-        self.fslider2 = FloatSlider(self, -1, 1, 1, 100, 1)
-        self.fslider3 = FloatSlider(self, -1, 1, 1, 3, 1)
-        self.txt0 = wx.StaticText(pnl, label='1')
-        self.txt1 = wx.StaticText(pnl, label='1')
-        self.txt2 = wx.StaticText(pnl, label='1')
-        self.txt3 = wx.StaticText(pnl, label='1')
-        pnl_sizer = wx.GridBagSizer(1, 1)
-        pnl_sizer.Add(wx.StaticText(pnl, label='FFT'), pos=(0, 0), flag=wx.LEFT, border=10)
-        pnl_sizer.Add(wx.StaticText(pnl, label='AMP'), pos=(3, 0), flag=wx.LEFT, border=10)
-        pnl_sizer.Add(wx.StaticText(pnl, label='FRQ'), pos=(5, 0), flag=wx.LEFT, border=10)
-        pnl_sizer.Add(wx.StaticText(pnl, label='ST'), pos=(7, 0), flag=wx.LEFT, border=10)
-        pnl_sizer.Add(self.txt0, pos=(1, 1), flag=wx.RIGHT, border=10)
-        pnl_sizer.Add(self.txt1, pos=(4, 1), flag=wx.RIGHT, border=10)
-        pnl_sizer.Add(self.txt2, pos=(6, 1), flag=wx.RIGHT, border=10)
-        pnl_sizer.Add(self.txt3, pos=(8, 1), flag=wx.RIGHT, border=10)
-        pnl_sizer.Add(self.fslider0, pos=(1, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.Add(self.fslider1, pos=(4, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.Add(self.fslider2, pos=(6, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.Add(self.fslider3, pos=(8, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.Add(self.button, pos=(2, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.Add(self.button1, pos=(9, 0), flag=wx.ALL | wx.EXPAND, border=1)
-        pnl_sizer.AddGrowableCol(0)
-        pnl_sizer.SetMinSize(200, 100)
-        pnl.SetSizer(pnl_sizer)
-        sizer.Add(pnl, 0, wx.SHAPED)
-        sizer.Add(self.nb, 1, wx.EXPAND)
-        self.SetSizer(sizer)
-
-    def RefVal(self, event):
-        self.txt0.SetLabel(str(self.fslider0.GetValue()))
-        self.txt1.SetLabel(str(self.fslider1.GetValue()))
-        self.txt2.SetLabel(str(self.fslider2.GetValue()))
-        self.txt3.SetLabel(str(self.fslider3.GetValue()))
-        self.fslider0.Bind(wx.EVT_SCROLL, self.fslider0._OnScroll)
-        self.fslider1.Bind(wx.EVT_SCROLL, self.fslider1._OnScroll)
-        self.fslider2.Bind(wx.EVT_SCROLL, self.fslider2._OnScroll)
-        self.fslider3.Bind(wx.EVT_SCROLL, self.fslider3._OnScroll)
-
-    def add(self, name="plot"):
-        page = Plot(self.nb)
-        self.nb.AddPage(page, name)
-        return page.figure
-
-    def delI(self, id):
-        self.nb.DeletePage(id)
+from matplotlib.pyplot import minorticks_on
+from mathfunc import (Meas, PlotNotebook, Plot, FloatSlider, switch_demo, FFT_Func)
 
 
 # Number of sample points
-N = 5000
+N = 2000
 # sample spacing
-T = 1.0 / 800.0
+T = 1.0 / 1000.0
 x = np.linspace(0.0, N * T, N, endpoint=False)
 
 
@@ -178,41 +18,50 @@ def demo():
     app = wx.App()
     frame = wx.Frame(None, -1, 'Plotter')
     plotter = PlotNotebook(frame)
-
-    P0=0
-    P1=0
+    btn = 0
 
     sd0 = plotter.fslider0.GetValue()
     sd1 = plotter.fslider1.GetValue()
     sd2 = plotter.fslider2.GetValue()
     sd3 = plotter.fslider3.GetValue()
+    y1 = switch_demo(2, 10, 2, 0, x)
+    y = switch_demo(sd3, sd2, sd1, sd3, x)
+    fft_ch = FFT_Func(y, N, T, sd0)
 
-    y = switch_demo(sd3, sd2, sd1, sd3)
-    yf = np.fft.fft(y)
-    xf = np.fft.fftfreq(N, T)[:N // 2]
+    def ValAct(sig):
+        y_meas = Meas(sig)
+        fft_ch = FFT_Func(sig, N, T/500, sd0)
+        max_v = np.argmax(fft_ch.FFT_Freq_Y())
+        max_f = (fft_ch.FFT_Freq()[max_v]) / (N * T)
+        duty = y_meas.PosDuty()
+        plotter.vpeak.SetLabel(str(y_meas.ValPeak()) + ' V')
+        plotter.vmax.SetLabel(str(y_meas.ValMax()) + ' V')
+        plotter.vmin.SetLabel(str(y_meas.ValMin()) + ' V')
+        plotter.vmid.SetLabel(str(y_meas.ValMid()) + ' V')
+        plotter.vavg.SetLabel(str(y_meas.ValAvg()) + ' V')
+        plotter.vacrms.SetLabel(str(y_meas.ValACRMS()) + ' Ṽ')
+        plotter.vdcrms.SetLabel(str(y_meas.ValDCRMS()) + ' Ṽ')
+        plotter.vamp.SetLabel(str(y_meas.ValAmp()) + ' V')
+        plotter.vover.SetLabel((str(y_meas.ValOver()) + ' %'))
+        plotter.vpduty.SetLabel(str(duty) + ' %')
+        plotter.vnduty.SetLabel(str(round(1 - duty, 4)) + ' %')
+        plotter.freq.SetLabel(str(max_f) + ' Hz')
 
-    res = max(20 / N * np.abs(yf[0:N // 2]))
-    z = argrelextrema(2.0 / N * np.abs(yf[0:N // 2]), np.greater, 0, sd0)
-    z = np.concatenate(np.asarray(z))
-    zy = np.ones(len(z)) * (2.0 / N * np.abs(yf[z]))
-    s = np.zeros(len(z))
+    ValAct(y)
 
     axes1 = plotter.add('Signal').gca()
     ax1 = 0
-    p1 = 0
-    print(plotter.idzed)
     axes1.plot(x, y)
-    axes1.plot(x, -y)
-    axes1.plot(x + 0.1, y - 0.5 * y)
+    axes1.minorticks_on()
     axes1.grid(True, which='both')
 
-    axes2 = plotter.add('FFT').gca()
+    axe2 = plotter.add('FFT').gca()
     ax2 = 1
-    print(plotter.idzed)
-    for i, txt in enumerate(z / (N * T)):
-        axes2.annotate(txt, ((z[i]) / (N * T), zy[i]))
-    axes2.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
-    axes2.grid(True, which='both')
+    for i, txt in enumerate(fft_ch.FFT_Freq() / (N * T)):
+        axe2.annotate(txt, ((fft_ch.FFT_Freq()[i]) / (N * T), fft_ch.FFT_Freq_Y()[i]))
+    axe2.plot(fft_ch.FFT_X(), 2.0 / N * np.abs(fft_ch.FFT_Y()[0:N // 2]))
+    axe2.minorticks_on()
+    axe2.grid(True, which='both')
 
     def onButton1(event):
         sd0 = plotter.fslider0.GetValue()
@@ -220,19 +69,13 @@ def demo():
         sd2 = plotter.fslider2.GetValue()
         sd3 = plotter.fslider3.GetValue()
 
-        y = switch_demo(sd3, sd2, sd1, sd3)
-        yf = np.fft.fft(y)
-        xf = np.fft.fftfreq(N, T)[:N // 2]
-
-        res = max(20 / N * np.abs(yf[0:N // 2]))
-        z = argrelextrema(2.0 / N * np.abs(yf[0:N // 2]), np.greater, 0, sd0)
-        z = np.concatenate(np.asarray(z))
-        zy = np.ones(len(z)) * (2.0 / N * np.abs(yf[z]))
-        s = np.zeros(len(z))
         nonlocal ax1
         nonlocal ax2
-        nonlocal P0
+        nonlocal y
 
+        y1 = switch_demo(2, 10, 2, 0, x)
+        y = switch_demo(sd3, sd2, sd1, sd3, x)
+        ValAct(y)
 
         plotter.delI(ax1)
         ax2 = 0
@@ -240,9 +83,10 @@ def demo():
 
         axe = plotter.add('Signal').gca()
         axe.plot(x, y)
-        axe.plot(x, -y)
-        axe.plot(x + 0.1, y - 0.5 * y)
-        axe.grid(True, which='both')
+        if btn == 1:
+            axe.plot(x, y1)
+        axe.minorticks_on()
+        axe.grid(True, which='both', axis='both')
 
     def onButton0(event):
         sd0 = plotter.fslider0.GetValue()
@@ -250,36 +94,46 @@ def demo():
         sd2 = plotter.fslider2.GetValue()
         sd3 = plotter.fslider3.GetValue()
 
-        y = switch_demo(sd3, sd2, sd1, sd3)
-        yf = np.fft.fft(y)
-        xf = np.fft.fftfreq(N, T)[:N // 2]
-
-        res = max(20 / N * np.abs(yf[0:N // 2]))
-        z = argrelextrema(2.0 / N * np.abs(yf[0:N // 2]), np.greater, 0, sd0)
-        z = np.concatenate(np.asarray(z))
-        zy = np.ones(len(z)) * (2.0 / N * np.abs(yf[z]))
-        s = np.zeros(len(z))
-
         nonlocal ax2
         nonlocal ax1
-        nonlocal P1
+        nonlocal y
+        y = switch_demo(sd3, sd2, sd1, sd3, x)
+        fft_ch = FFT_Func(y, N, T, sd0)
 
 
         plotter.delI(ax2)
         ax1 = 0
         ax2 = 1
         axe2 = plotter.add('FFT').gca()
-        for i, txt in enumerate(z / (N * T)):
-            axe2.annotate(txt, ((z[i]) / (N * T), zy[i]))
-        axe2.plot(xf, 2.0 / N * np.abs(yf[0:N // 2]))
-        axe2.grid(True, which='both')
+        for i, txt in enumerate(fft_ch.FFT_Freq() / (N * T)):
+            axe2.annotate(txt, ((fft_ch.FFT_Freq()[i]) / (N * T), fft_ch.FFT_Freq_Y()[i]))
+        axe2.plot(fft_ch.FFT_X(), 2.0 / N * np.abs(fft_ch.FFT_Y()[0:N // 2]))
+        axe2.minorticks_on()
+        axe2.grid(True, which='both', axis='both')
+
+    def onButton2(event):
+        nonlocal btn
+        btn ^= 1
+        plotter.txt4.SetLabel(str(btn))
+
+    def ChngChnl1(event):
+        ValAct(y)
+        plotter.chnlid.SetLabel(str('CH1'))
+
+    def ChngChnl2(event):
+        ValAct(y1)
+        plotter.chnlid.SetLabel(str('CH2'))
 
     plotter.button.Bind(wx.EVT_BUTTON, onButton0)
     plotter.button1.Bind(wx.EVT_BUTTON, onButton1)
+    plotter.button2.Bind(wx.EVT_BUTTON, onButton2)
+    plotter.btnch1.Bind(wx.EVT_BUTTON, ChngChnl1)
+    plotter.btnch2.Bind(wx.EVT_BUTTON, ChngChnl2)
     plotter.fslider0.Bind(wx.EVT_SCROLL, plotter.RefVal)
     plotter.fslider1.Bind(wx.EVT_SCROLL, plotter.RefVal)
     plotter.fslider2.Bind(wx.EVT_SCROLL, plotter.RefVal)
     plotter.fslider3.Bind(wx.EVT_SCROLL, plotter.RefVal)
+
     frame.Show()
     app.MainLoop()
 
